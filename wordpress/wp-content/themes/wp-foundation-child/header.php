2141f82bdc2369c4f5ab7092f7dc403068ee19a4
Incunabula01 <JWidener08@gmail.com>
2014-03-17 14:06:44 -0400

    Updated Nav Bar (screen)

--- wordpress/wp-content/themes/wp-foundation-child/header.php
+++ wordpress/wp-content/themes/wp-foundation-child/header.php
@@ -133,15 +133,19 @@
 					</div>
 
 					<?php bones_mobile_nav(); ?>
+
 					<div class="siteinfo">
 						<div id="main-logo">
-							<h1>
-								<a class="brand" id="logo" href="<?php echo get_bloginfo('url'); ?>">
-									<img src="BmoreUnsceneLogo.png" border=0 alt="Baltimore Unscene"/>
-								</a>
+							<a class="brand" id="logo" href="<?php echo get_bloginfo('url'); ?>">
+							<div id="logo-icon">
+							</div>
+							</a>
+								<h1>
 									<?php bloginfo('name'); ?>
-							</h1>
-							<h4 class="subhead"><?php echo get_bloginfo ( 'description' ); ?></h4>
+								</h1>
+								<h4 class="subhead">
+									<?php echo get_bloginfo ( 'description' ); ?>
+								</h4>
 						</div>
 					</div>
 			
