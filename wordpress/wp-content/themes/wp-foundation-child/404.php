acb83e6e8b850ece0b0d9668bf4f677e0c806d47
Incunabula01 <JWidener08@gmail.com>
2014-05-07 20:55:07 -0400

    Finished 404 page

---
+++ wordpress/wp-content/themes/wp-foundation-child/404.php
@@ -0,0 +1,32 @@
+<?php get_header(); ?>
+			
+			<div id="content" class="clearfix">
+			
+				<div id="main" class="twelve columns clearfix" role="main">
+
+					<article id="post-not-found" class="clearfix">
+						
+						<div class="error-image"></div>
+						
+						<header>
+							
+							<h1>Epic 404 - Article Not Found</h1>
+						
+						</header> <!-- end article header -->
+
+						<section class="post_content">
+								<p>Oops, the article you were looking for was not found, but maybe try looking again!</p>
+						</section> <!-- end article section -->
+
+						
+						<footer>
+							
+						</footer> <!-- end article footer -->
+					
+					</article> <!-- end article -->
+			
+				</div> <!-- end #main -->
+    
+			</div> <!-- end #content -->
+
+<?php get_footer(); ?>
